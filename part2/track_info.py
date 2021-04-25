96d7dd4dcdc4896475aa93d9ecf58ba9e7c74cf7
jallwork <35535813+jallwork@users.noreply.github.com>
2021-04-25 14:19:12 +0100

    Add files via upload
    Adding files needed for Alexa hosted skill for part2

---
+++ part2/track_info.py
@@ -0,0 +1,26 @@
+track_info = [
+{
+    "genre" : "Blues",
+    "title" : "You Left all the Water running",
+    "artist" :"Removal Men",
+    "url" : "Media/RemovalMen_YouLeftAllTheWaterRunning.mp3"
+},
+{
+    "genre": "Rock",
+    "title" : "Tell Momma",
+    "artist" :"Removal Men",
+    "url" :"Media/RemovalMen_TellMomma.mp3"
+},
+{
+    "genre":"Blues",
+    "title" :"Cant find my way home",
+    "artist" : "Mandy Montgomery",
+    "url" :"Media/MandyMontgomery_CantFindMyWayHome.mp3"
+},
+{
+    "genre" :"Rock",
+    "title" : "Tumbling dice",
+    "artist" : "Mandy Montgomery",
+    "url" : "Media/MandyMontgomery_TumblingDice.mp3"
+}
+]
